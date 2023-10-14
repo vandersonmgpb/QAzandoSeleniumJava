1d0908b94b42b9bdcca21db460f06bb46a9b07db
Vanderson1022 <vanderson1022@gmail.com>
2023-10-14 08:06:37 -0300

    Hooks: BeforeClass e AfterClass, organizando o teste

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -2,7 +2,11 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -13,6 +17,16 @@ import org.junit.runner.RunWith;
 )
 public class RunCucumberTest {
 
+    public static WebDriver driver;
 
+    @BeforeClass
+    public static void start(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public static void stop(){
+        driver.quit();
+    }
 
 }
--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -3,15 +3,19 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class DescontosStep {
-    WebDriver driver = new ChromeDriver();
+import runner.RunCucumberTest;
+
+public class DescontosStep extends RunCucumberTest {
+
     @Dado("^que estou no site da qazando$")
     public void acessar_site_qazando() {
         driver.get("https://qazando.com.br/curso.html");
+        Assert.assertEquals("Não acessou a aplicação!", true, driver.findElement(By.id("btn-ver-cursos")).isDisplayed());
     }
     @Quando("^preencho meu e-mail$")
     public void preencho_meu_e_mail() throws InterruptedException {
@@ -29,8 +33,9 @@ public class DescontosStep {
     @Então("^eu vejo o código de desconto$")
         public void eu_vejo_o_codigo_de_desconto(){
             String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
-            System.out.println(texto_cupom);
 
-            driver.quit();
+            Assert.assertEquals("O cupom está errado!", "QAZANDO15OFF", texto_cupom);
+
+
     }
 }
