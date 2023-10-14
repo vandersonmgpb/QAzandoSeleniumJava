66ff94f8867e20b52d60fba860aa233ab437cfc7
Vanderson1022 <vanderson1022@gmail.com>
2023-10-14 07:21:37 -0300

    Escrevendo ações do BDD

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -3,28 +3,34 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class DescontosStep {
-    @Dado("^que estou no site da qazando")
-    public void acessar_site_qazando(){
-        System.out.println("Passou1");
+    WebDriver driver = new ChromeDriver();
+    @Dado("^que estou no site da qazando$")
+    public void acessar_site_qazando() {
+        driver.get("https://qazando.com.br/curso.html");
     }
-
     @Quando("^preencho meu e-mail$")
-    public void preencho_meu_e_mail() {
-        System.out.println("Passou2");
-    }
+    public void preencho_meu_e_mail() throws InterruptedException {
 
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollTo(0, 10000)");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("email")).sendKeys("email@email.com.br");
+    }
     @Quando("^clico em ganhar cupom$")
     public void clico_em_ganhar_cupom() {
-        System.out.println("Passou3");
+        driver.findElement(By.id("button")).click();
     }
-
     @Então("^eu vejo o código de desconto$")
-    public void eu_vejo_o_código_de_desconto()  {
-        System.out.println("Passou4");
+        public void eu_vejo_o_codigo_de_desconto(){
+            String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+            System.out.println(texto_cupom);
+
+            driver.quit();
     }
-
-
-
 }
